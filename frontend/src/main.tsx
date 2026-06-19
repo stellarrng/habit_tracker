90787debab9d9235c560ed3ac63c5863bb3d3814
hly <lyminhhanh2002@gmail.com>
2026-06-20 04:11:44 +0700

    Refactor CSS variables for improved organization and clarity

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./styles/variables.css";
 import "./styles/globals.css";
 import App from "./App";
-import "./styles/global.css";
-import "./styles/variables.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
